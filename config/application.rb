fcdb120e145fcdaecc690fb9fa2fe95ca65a0feb
Dave Greig <oxnard@tpg.com.au>
2017-04-21 22:06:37 +1000

    Adds precompile assets instruction

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.precompile += Ckeditor.assets
 
 module Typesomethinghere
   class Application < Rails::Application
