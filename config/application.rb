fe4e94557b40defaa0a726c43bd3a1497ab85154
Dave Greig <oxnard@tpg.com.au>
2017-04-21 22:08:27 +1000

    Done goofed on last commit.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.precompile += Ckeditor.assets
+
 
 module Typesomethinghere
   class Application < Rails::Application
@@ -23,5 +23,6 @@ module Typesomethinghere
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += Ckeditor.assets
   end
 end
