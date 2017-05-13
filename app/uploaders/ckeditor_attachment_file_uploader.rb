1f9c97d667dc407110f5999853be4b1f70c4e88b
Dave Greig <oxnard@tpg.com.au>
2017-05-13 22:52:31 +1000

    Changes upload path to public folder for production env take 2

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -15,7 +15,7 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "public/uploads/ckeditor/attachments/#{model.id}"
+    "/public/uploads/ckeditor/attachments/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
