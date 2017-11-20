f92512c5e6e073b6db87f51d392a0d3ad59e08ca
Dave Greig <oxnard@tpg.com.au>
2017-11-20 20:57:42 +1100

    Revert changes for devise as they are not called

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def after_sign_in_path_for(resource)
-    sign_in_url = new_admin_session_url
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
-  end
 
   #configure permitted parameters for devise
   def configure_permitted_parameters
